b7fe88e897097682534a5073c980aba5911db1f4
dnsi37 <dnsi37@naver.com>
2022-07-13 21:54:56 +0900

    Add Router Receiver

--- HomeIoT/main.go
+++ HomeIoT/main.go
@@ -12,7 +12,6 @@ import (
 
 func main() {
 	
-	
 	ConnectToAWSIoT()
 	server.Init()
 	
--- HomeIoT/server/routes.go
+++ HomeIoT/server/routes.go
@@ -1,9 +1,36 @@
 package server
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+)
 
 type Router struct {
-	BaseURL string
+	baseURL string
+}
+
+func (r *Router) NewRouter (baseUrl string) *Router {
+	router := Router{ baseURL:baseUrl }
+	return &router
+}
+
+func (r *Router) SetBaseURL (baseUrl string) {
+	r.baseURL = baseUrl
+}
+
+func (r *Router) ValidateBaseURL () {
+	err := errors.New("INVALID_BASE_URL")
+
+	newSlice := []rune(r.baseURL)
+
+	if newSlice[0] != '/' {
+		panic (err)
+	}
+
+	/*if newSlice[len(newSlice)-1] != '/' {
+		panic (err)
+	}*/
+
 }
 
 func (r *Router) GetHandler(path string ,  newhandler func(http.ResponseWriter, *http.Request) ) {
@@ -13,6 +40,46 @@ func (r *Router) GetHandler(path string ,  newhandler func(http.ResponseWriter, 
 		newhandler(w,req)
 	 }
 	}
-	url := r.BaseURL + path
+	r.ValidateBaseURL()
+	url := r.baseURL + path
 	http.HandleFunc(url,getHandler)
 }
+
+func (r *Router) PostHandler (path string ,  newhandler func(http.ResponseWriter, *http.Request) ) {
+
+	postHandler := func(w http.ResponseWriter, req *http.Request){
+	 if req.Method==http.MethodPost {
+		newhandler(w,req)
+	 }
+	}
+	r.ValidateBaseURL()
+	url := r.baseURL + path
+	http.HandleFunc(url,postHandler)
+}
+
+func (r *Router) DeleteHandler (path string ,  newhandler func(http.ResponseWriter, *http.Request) ) {
+
+	DeleteHandler := func(w http.ResponseWriter, req *http.Request){
+	 if req.Method==http.MethodPost {
+		newhandler(w,req)
+	 }
+	}
+	r.ValidateBaseURL()
+	url := r.baseURL + path
+	http.HandleFunc(url,DeleteHandler)
+}
+
+
+func (r *Router) PatchHandler (path string ,  newhandler func(http.ResponseWriter, *http.Request) ) {
+
+	PatchHandler := func(w http.ResponseWriter, req *http.Request){
+	 if req.Method==http.MethodPatch {
+		newhandler(w,req)
+	 }
+	}
+	r.ValidateBaseURL()
+	url := r.baseURL + path
+	http.HandleFunc(url,PatchHandler)
+}
+
+ 
